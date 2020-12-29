8018ffe6644ca787987dd382a4e9fb27647e133c
New Future (Weilai LIU) <New.Future@microsoft.com>
2020-12-29 21:10:29 +0800

    add version

--- bin/package-teams-app.js
+++ bin/package-teams-app.js
@@ -5,15 +5,14 @@
 const fs = require('fs');
 const path = require("path");
 
-const initTemplate = require("../src/init");
-const packageTeamsApp = require("../src/index");
-
+const pkg = require("../package.json");
 const helpMessage = `
 
+====================    package-teams-app v${pkg.version}    ====================
 Usage:
-  package-teams-app [inputFolder] [outputFile]    Pacakge your manifest folder.
-  package-teams-app --init [initDir]              Init an template manifest folder.
-  package-teams-app --help                        Print help messages.
+  package-teams-app [inputDir] [outputFile]    Pacakge your manifest folder.
+  package-teams-app --init [initDir]           Init an template manifest folder.
+  package-teams-app --help                     Print help messages.
 
 Examples:
   package-teams-app manifest
@@ -30,22 +29,23 @@ const defaultDir = "manifest";
 const args = process.argv.slice(2);
 
 
-
 if (args.length == 0 && fs.existsSync(defaultDir)) {
   args[0] = defaultDir;
 }
 
 if (args.length == 0 || args[0] == "--help" || args[0] == "-h") {
-  const intro = "Package Micosoft Teams app manifest.\n All the Template strings in json files will be replaced by environment variables."
+  const intro = "Package Micosoft Teams app manifest."
   console.log(intro + helpMessage)
 } else if (args[0] == "--init" || args[0] == "-i") {
   const initDir = args[1] || defaultDir
+  const initTemplate = require("../src/init");
   console.log("init", initDir);
   initTemplate(initDir).then(() => console.log("Template manifest files copy to", initDir));
 } else if (!fs.existsSync(args[0])) {
   console.error(args[0], "not exists!" + helpMessage);
   process.exit(1);
 } else {
+  const packageTeamsApp = require("../src/index");
   const src = args[0];
   packageTeamsApp(src, args[1])
     .then(outfile => console.log("Package manifest:", src, "==>", outfile));
